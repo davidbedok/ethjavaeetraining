8f8922010013ddf35baf971ace4644413db42143
edvibed <david.bedok@ericsson.com>
2017-05-20 15:43:23 +0200

    ADD: lab 08 task 01

---
+++ coininventory/lab-08-task-01/ci-ejbservice/src/main/java/com/ericsson/ci/ejbservice/interceptor/CountryCode.java
@@ -0,0 +1,14 @@
+package com.ericsson.ci.ejbservice.interceptor;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.PARAMETER })
+public @interface CountryCode {
+
+}
